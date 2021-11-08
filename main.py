8c2dbdf283661db163d4f9e66edc20cca29bae05
suhail1796 <86944954+suhail1796@users.noreply.github.com>
2021-11-08 10:59:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 '''
-
-                            Online Python Compiler.
-                Code, Compile, Run and Debug python program online.
-Write your code in this editor and press "Run" button to execute it.
+GYM MANAGEMENT APPLICATION
+PREPARED BY: SUHAIL SHAIKH
 
 '''
 
